efce659cbe24b1feddaa8c75c26fddccd7d5a01d
Dennis Calderón <developergbp48@gmail.com>
2022-03-03 17:35:46 -0500

    video 115 mensajes en salas p

--- sockets/socket.js
+++ sockets/socket.js
@@ -6,7 +6,7 @@ const {usuarioConectado, usuarioDesconectado} = require('../controllers/socket')
 // Mensajes de Sockets
 io.on('connection',(client) => {
     
-    console.log('Cliente conectado');
+    //console.log('Cliente conectado');
     //cliente con JWT
     const [valido, uid] = comprobarJWT (client.handshake.headers['x-token']);
 
@@ -16,17 +16,28 @@ io.on('connection',(client) => {
     //Cliente Autenticado //console.log('cliente autenticado')
     usuarioConectado(uid);
 
+    //Ingresar al usuario a una sala en particular
+    //sala global, client.id, 
+    client.join(uid);
+
+    //escuchar del cliente el mensaje-personal
+    client.on('mensaje-personal', (payload) =>{
+        console.log(payload);
+
+        io.to(payload.para).emit('mensaje-personal', payload);
+    })
+    
+
     client.on('disconnect', () => {
         //console.log('Cliente desconectado');
         usuarioDesconectado(uid);
     });
 
-    client.on('mensaje', ( payload ) => {
-        console.log('Mensaje', payload);
+    // client.on('mensaje', ( payload ) => {
+    //     console.log('Mensaje', payload);
 
-        io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
+    //     io.emit( 'mensaje', { admin: 'Nuevo mensaje' } );
 
-    });
-
+    // });
 
 });
