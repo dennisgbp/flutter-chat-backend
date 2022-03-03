406e01ee4cbbc49f804bf4150c0abf250bcfab97
Dennis Calderón <developergbp48@gmail.com>
2022-03-03 11:58:23 -0500

    DB actualiza si usuario se conecta vd 109 sección 10

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,12 +1,24 @@
+const { comprobarJWT } = require('../helpers/jwt');
 const { io } = require('../index');
+const {usuarioConectado, usuarioDesconectado} = require('../controllers/socket');
 
 
 // Mensajes de Sockets
-io.on('connection', client => {
+io.on('connection',(client) => {
+    
     console.log('Cliente conectado');
+    //cliente con JWT
+    const [valido, uid] = comprobarJWT (client.handshake.headers['x-token']);
+
+    //Verificar  autenticación
+    if( !valido ) { return client.disconnect();}
+    
+    //Cliente Autenticado //console.log('cliente autenticado')
+    usuarioConectado(uid);
 
     client.on('disconnect', () => {
-        console.log('Cliente desconectado');
+        //console.log('Cliente desconectado');
+        usuarioDesconectado(uid);
     });
 
     client.on('mensaje', ( payload ) => {
